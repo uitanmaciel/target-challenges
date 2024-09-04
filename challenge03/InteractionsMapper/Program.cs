21712a5f2e1acde9e65aa307de8d51dee056f109
uitanmaciel <uitan.s.maciel@gmail.com>
2024-09-04 11:03:51 -0300

    :sparkles: feat: Implementado Desafio 03 - Valor da SOMA após um sequência de interações

---
+++ challenge03/InteractionsMapper/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
+Ao final do processamento, qual será o valor da variável SOMA?*/
+
+int indice = 12, soma = 0, k = 1;
+
+while (k < indice)
+{    
+    k++;
+    Console.WriteLine($"Interação {k - 1}: \nK = K({k - 1}) + 1 = {k}\nsoma = {soma} + {k} = {soma + k}\n");
+    soma += k;
+}
+
+Console.WriteLine($"O valor da variável SOMA é {soma}.");
