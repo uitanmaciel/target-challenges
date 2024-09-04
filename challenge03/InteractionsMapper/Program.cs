1ba92d794a4e07a2f53019020d7ea8f0aafa0967
uitanmaciel <uitan.s.maciel@gmail.com>
2024-09-04 16:32:11 -0300

    :recycle: refactor: Refatorado IntractionsMapper project - Mudança de nome de variável.

--- challenge03/InteractionsMapper/Program.cs
+++ challenge03/InteractionsMapper/Program.cs
@@ -1,13 +1,13 @@
 ﻿/*Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
 Ao final do processamento, qual será o valor da variável SOMA?*/
 
-int indice = 12, soma = 0, k = 1;
+int index = 12, sum = 0, k = 1;
 
-while (k < indice)
+while (k < index)
 {    
     k++;
-    Console.WriteLine($"Interação {k - 1}: \nK = K({k - 1}) + 1 = {k}\nsoma = {soma} + {k} = {soma + k}\n");
-    soma += k;
+    Console.WriteLine($"Interação {k - 1}: \nK = K({k - 1}) + 1 = {k}\nsoma = {sum} + {k} = {sum + k}\n");
+    sum += k;
 }
 
-Console.WriteLine($"O valor da variável SOMA é {soma}.");
+Console.WriteLine($"O valor da variável SOMA é {sum}.");
